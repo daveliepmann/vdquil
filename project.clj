d250ce460cd4129583bd97418e4ce47352fb8557
Dave Liepmann <dave.liepmann@gmail.com>
2013-09-24 12:09:09 +0300

    Let there be (light)

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject vdquil "0.1.0-SNAPSHOT"
+  :description "Ben Fry's *Visualizing Data* exercises, in Quil"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [quil "1.6.0"]])
---
+++ test/vdquil/core_test.clj
@@ -0,0 +1,7 @@
+(ns vdquil.core-test
+  (:use clojure.test
+        vdquil.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
