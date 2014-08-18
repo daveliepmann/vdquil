1289bc5da1b710e9cb67d9b3d886f48168e31447
mabrams <mabrams@uab.edu>
2014-08-18 13:03:53 -0500

    Updated Quil and clj-time version numbers

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject vdquil "0.1.0-SNAPSHOT"
+(defproject vdquil "0.1.1-SNAPSHOT"
   :description "Ben Fry's *Visualizing Data* exercises (in Quil!)"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["treemap"] ;; for Chapter 7's java interop work
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [quil "1.6.0"]
-                 [clj-time "0.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [quil "2.2.1"]
+                 [clj-time "0.8.0"]]
   :jvm-opts ["-Xmx4g"]) 
