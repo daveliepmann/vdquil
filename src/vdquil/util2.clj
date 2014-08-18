f0cacdb55de038e8c30f6c7edf6665cc9ec2ac57
mabrams <mabrams@uab.edu>
2014-08-17 23:19:45 -0500

    Extra generic convenience utilities

---
+++ src/vdquil/util2.clj
@@ -0,0 +1,13 @@
+(ns vdquil.util2)
+
+(defn unlocknload 
+  "Given a symbol representing a namespace, converts the symbol
+  into the corresponding path + clojure source fileanem, tries to 
+  load the file, and then uses (\"unlocks\") the namespace."
+  [nssym]
+  (load-file 
+    (str "src/" 
+         (clojure.string/replace (clojure.string/replace nssym \. \/) \- \_) 
+         ".clj"))
+    (use nssym))
+
