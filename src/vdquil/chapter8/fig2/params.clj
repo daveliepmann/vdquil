70f23bd04c671364bf032fadc5cd03e34d989267
mabrams <mabrams@uab.edu>
2014-08-19 22:46:08 -0500

    Added color defs.

--- src/vdquil/chapter8/fig2/params.clj
+++ src/vdquil/chapter8/fig2/params.clj
@@ -3,3 +3,8 @@
 (def ^:constant +width+ 720.0)
 (def ^:constant +height+ 405.0)
 
+(def ^:constant node-color   0xF0C070)
+(def ^:constant select-color 0xFF3030)
+(def ^:constant fixed-color  0xFF8080)
+(def ^:constant edge-color   0x000000)
+
